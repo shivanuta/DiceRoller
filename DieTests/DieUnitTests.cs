edb918865d0b29b6406ede672bec903a9ee7edfe
Shiva Rama Krishna Nutakki <nutakki.shivaramakrishna99@gmail.com>
2022-08-10 02:10:14 -0400

    Chnaged applied on TestMethod DieHasCustomSides.

--- DieTests/DieUnitTests.cs
+++ DieTests/DieUnitTests.cs
@@ -29,20 +29,29 @@ namespace DieTests
             def.CurrentSide.Should().BeInRange(1, 6);
         }
         [TestMethod]
-        [DataRow(3, 3)]
-        [DataRow(4, 4)]
-        [DataRow(8, 8)]
-        [DataRow(10, 10)]
-        [DataRow(12, 12)]
-        [DataRow(20, 20)]
-        public void DieHasCustomSides(int sides, int expected)
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(8)]
+        [DataRow(10)]
+        [DataRow(12)]
+        [DataRow(20)]
+        public void DieHasCustomSides(int sides)
         {
-            Die d = new Die(8);
-            d.Name.Should().Be("d8");
-            d.NumSides.Should().Be(8);
-            d.CurrentSide.Should().BeInRange(1, 8);
+            Die d = new Die(sides);
+            d.Name.Should().Be("d" + sides);
+            d.NumSides.Should().Be(sides);
+            d.CurrentSide.Should().BeInRange(1, sides);
         }
-
+        [TestMethod]
+        public void DieHasCustomName()
+        {
+            //not implemented
+        }
+        [TestMethod]
+        public void SetSideUpChangesSide()
+        {
+            //not implemented
+        }
 
         [TestMethod]
         public void NumSidesShouldNotBeNegative()
