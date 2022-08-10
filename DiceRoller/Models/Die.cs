f62a57c70981ee0a2403d7c56d415e21a7f00dab
Shiva Rama Krishna Nutakki <nutakki.shivaramakrishna99@gmail.com>
2022-08-10 01:44:28 -0400

    Implement TestMethod for DefaultValues.

--- DiceRoller/Models/Die.cs
+++ DiceRoller/Models/Die.cs
@@ -6,5 +6,35 @@ namespace DiceRoller.Models
 {
     public class Die
     {
+        //name ,type,or colout of the die
+
+        public String Name { get; set; }
+
+        //how many sides are the die
+
+        public int NumSides { get; set; }
+
+        //which number is currently up
+
+        public int CurrentSide { get; set; }
+
+        public Die()
+        {
+            NumSides = 6;
+            Name = "d6";
+            Roll();
+        }
+        
+        public Die(int numSides)
+        {
+            NumSides = numSides;
+            Name = "d" + numSides;
+            Roll();
+        }
+        public void Roll()
+        {
+            Random r = new Random();
+            CurrentSide = r.Next(NumSides) + 1;
+        }
     }
 }
--- DieTests/DieUnitTests.cs
+++ DieTests/DieUnitTests.cs
@@ -8,11 +8,25 @@ namespace DieTests
     [TestClass]
     public class DieUnitTests
     {
+        private Die def = new Die();
         [TestMethod]
-        public void TestMethod1()
+        public void DieNotNull()
         {
             Die d = new Die();
             d.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void DieHasDefaultValues()
+        {
+            /*
+             * Default value should be:
+             * name:....d6
+             /* numsides:.....6 
+            */
+            def.Name.Should().Be("d6");
+            def.NumSides.Should().Be(6);
+            def.CurrentSide.Should().BeInRange(1, 6);
         }
     }
 }
