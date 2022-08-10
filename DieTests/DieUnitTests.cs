9effc19aeb20b2337538820d31c0c4a5224d6ae3
Shiva Rama Krishna Nutakki <nutakki.shivaramakrishna99@gmail.com>
2022-08-10 01:46:15 -0400

    Implemented DieHasCustomSides.

--- DieTests/DieUnitTests.cs
+++ DieTests/DieUnitTests.cs
@@ -28,5 +28,19 @@ namespace DieTests
             def.NumSides.Should().Be(6);
             def.CurrentSide.Should().BeInRange(1, 6);
         }
+        [TestMethod]
+        [DataRow(3, 3)]
+        [DataRow(4, 4)]
+        [DataRow(8, 8)]
+        [DataRow(10, 10)]
+        [DataRow(12, 12)]
+        [DataRow(20, 20)]
+        public void DieHasCustomSides(int sides, int expected)
+        {
+            Die d = new Die(8);
+            d.Name.Should().Be("d8");
+            d.NumSides.Should().Be(8);
+            d.CurrentSide.Should().BeInRange(1, 8);
+        }
     }
 }
